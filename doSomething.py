7f08338b1621d900d3147d9c9d5361f963e289b4
Brent Brewster <112504532+Brent-Brewster@users.noreply.github.com>
2023-02-20 18:46:15 +0000

    added something

--- doSomething.py
+++ doSomething.py
@@ -5,3 +5,8 @@ age = input("Please enter your age: ")
 print("age: " + age)
 
 # group members, the next task is "do something with this input." Create a pull request with code that will do something
+
+if int(age) < 16:
+    print("Sorry you are to young to drive, " + name)
+else:
+    print("Enjoy your drive, " + name)
