f1f6165591c003d51c134ad023d5717d2a7c924a
Aidan <75161978+Razzi86@users.noreply.github.com>
2023-02-20 13:04:21 -0500

    Create doSomething.py
    initial code

---
+++ doSomething.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name: ")
+print("name: " + name)
+
+age = input("Please enter your age: ")
+print("age: " + age)
+
+# group members, the next task is "do something with this input." Create a pull request with code that will do something
